cf929a2019583e78a4727b8eae8183bbbfd8d8a9
Patrick Lim <patrick.lim@medsphere.com>
2021-03-03 22:29:16 +0000

    Saving wiki attachment 'ADD_SERVICE_TO_SISLNK_DB-081f0e8b-3239-4eed-aac0-1f6a43fb5f97.sql'.

---
+++ .attachments/ADD_SERVICE_TO_SISLNK_DB-081f0e8b-3239-4eed-aac0-1f6a43fb5f97.sql
@@ -0,0 +1,37 @@
+
+
+select * from CLIENTS -- Use to determine client ids for current source/dest
+
+SELECT * FROM record_routing where source = '200' and destination = '300' AND record_type in ('2701', '2702', '2711')
+
+select * from record_types where record_type in ('2701', '2702', '2711')
+
+
+
+begin transaction
+
+DECLARE @serviceName char(32)
+SET @serviceName = 'ImageHL7ToEngineHL7'
+
+DECLARE @serviceId int
+SET @serviceId = 310 -- Verify this id is not being used
+
+INSERT INTO CLIENTS(client_name, description, client_num, record_transmitted, current_record)
+VALUES(@serviceName, @serviceName, @serviceId, '0', null)
+
+UPDATE record_routing
+SET destination = @serviceId
+WHERE source = '200' AND destination = '300' AND record_type in ('2701', '2702', '2711')
+
+SELECT * FROM record_routing WHERE source = '200' AND record_type in ('2701', '2702', '2711')
+
+rollback
+
+commit
+
+
+
+
+
+
+
